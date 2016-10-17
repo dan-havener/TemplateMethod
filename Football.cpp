3753b34a59eaf5746083bdeddfbefb858b6c15a3
SamuelPoquette <samuel.poquette@mymail.champlain.edu>
2016-10-17 17:45:58 -0400

    Added Baseball, and added output for the specific functions

---
+++ Football.cpp
@@ -0,0 +1,27 @@
+#include "Football.h"
+
+
+Football::Football()
+{
+}
+
+Football::~Football()
+{
+}
+
+void Football::setUpGame()
+{
+	printf("Football match set up \n")
+}
+
+void Football::startGame()
+{
+	printf("Football match started \n")
+
+}
+
+void Football::endGame()
+{
+	printf("Football match ended! \n\n")
+
+}
